b7f0c5480324cce22d7a4239887e448b8339650e
Wikan <wikan25@gmail.com>
2026-07-18 14:53:02 +0700

    Fix mine_variant_keywords.sql to exclude empty-string variants

--- sql/queries/mine_variant_keywords.sql
+++ sql/queries/mine_variant_keywords.sql
@@ -7,5 +7,5 @@ INSERT INTO `sincere-hearth-273704.magpie_reference.brand_variant_keywords`
   (brand_id, variant, product_count, computed_at)
 SELECT brand_id, variant, COUNT(*) AS product_count, CURRENT_TIMESTAMP()
 FROM `sincere-hearth-273704.magpie_reference.product_taxonomy`
-WHERE variant IS NOT NULL
+WHERE variant IS NOT NULL AND TRIM(variant) != ''
 GROUP BY brand_id, variant;
